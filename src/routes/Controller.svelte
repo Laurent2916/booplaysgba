ba419485c8f3fd0178c7883c78ba4697d66c08c2
Laureηt <laurentfainsin@protonmail.com>
2021-12-13 18:34:20 +0100

    feat: converted admin.html to Admin.svelte
    file is broken, need refactor

--- src/routes/Controller.svelte
+++ src/routes/Controller.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-  import { onMount } from "svelte";
+  import { onMount } from 'svelte';
 
   let websocket: WebSocket | undefined;
 
   onMount(() => {
-    websocket = new WebSocket("ws://localhost:6789/");
+    websocket = new WebSocket('ws://localhost:6789/');
   });
 
   const sendAction = (key: string) => () => {
@@ -12,38 +12,40 @@
   };
 </script>
 
+<title>Télécommande</title>
+
 <table class="buttons">
   <tr>
     <td colspan="2" style="padding-bottom: 1rem;">
-      <button id="l" on:click={sendAction("l")}>L</button>
+      <button id="l" on:click={sendAction('l')}>L</button>
     </td>
     <td />
     <td colspan="2" style="padding-bottom: 1rem; text-align: right;">
-      <button id="r" on:click={sendAction("r")}>R</button>
+      <button id="r" on:click={sendAction('r')}>R</button>
     </td>
   </tr>
 
   <tr>
     <td />
     <td>
-      <button id="up" on:click={sendAction("up")}> ∧ </button>
+      <button id="up" on:click={sendAction('up')}> ∧ </button>
     </td>
     <td />
     <td>
-      <button id="select" on:click={sendAction("select")}> select </button>
+      <button id="select" on:click={sendAction('select')}> select </button>
     </td>
     <td>
-      <button id="start" on:click={sendAction("start")}> start </button>
+      <button id="start" on:click={sendAction('start')}> start </button>
     </td>
   </tr>
 
   <tr>
     <td>
-      <button id="left" on:click={sendAction("left")}> &lt; </button>
+      <button id="left" on:click={sendAction('left')}> &lt; </button>
     </td>
     <td />
     <td style="padding-right: 20%;">
-      <button id="right" on:click={sendAction("right")}> > </button>
+      <button id="right" on:click={sendAction('right')}> > </button>
     </td>
     <td />
     <td />
@@ -52,19 +54,19 @@
   <tr>
     <td />
     <td>
-      <button id="down" on:click={sendAction("down")}> ∨ </button>
+      <button id="down" on:click={sendAction('down')}> ∨ </button>
     </td>
     <td />
     <td>
-      <button id="a" on:click={sendAction("a")}>A</button>
+      <button id="a" on:click={sendAction('a')}>A</button>
     </td>
     <td>
-      <button id="b" on:click={sendAction("b")}>B</button>
+      <button id="b" on:click={sendAction('b')}>B</button>
     </td>
   </tr>
 </table>
 
-<style lang="sass">
+<style lang="scss">
   td {
     text-align: center;
   }
